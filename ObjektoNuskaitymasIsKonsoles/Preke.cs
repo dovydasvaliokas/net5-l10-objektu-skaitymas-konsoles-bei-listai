14ffe9acdccaf2f670bf9725a41d6360323da77f
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-05-30 12:07:29 +0300

    Sukuriau Prekes klase. Sukuriau funkcija, kuri nuskaito Prekes klasess objekta is konsoles. Sukuriau funkcija, kuri isveda Prekes objekta i konsole.

---
+++ ObjektoNuskaitymasIsKonsoles/Preke.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektoNuskaitymasIsKonsoles
+{
+    internal class Preke
+    {
+        private string pavadinimas;
+        private string aprasymas;
+        private double kaina;
+        private long id;
+
+        public Preke(string pavadinimas, string aprasymas, double kaina, long id)
+        {
+            this.pavadinimas = pavadinimas;
+            this.aprasymas = aprasymas;
+            this.kaina = kaina;
+            this.id = id;
+        }
+
+        public Preke()
+        {
+        }
+
+        public string Pavadinimas 
+        { 
+            get 
+            { 
+                return pavadinimas; 
+            }
+            set
+            {
+                this.pavadinimas = value;   
+            }
+        }
+
+        public string Aprasymas
+        {
+            get
+            {
+                return aprasymas;
+            }
+            set { aprasymas = value; }
+        }
+
+        public long Id
+        {
+            get
+            {
+                return id;  
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public double Kaina
+        {
+            get { return kaina; }
+            set { kaina = value; }
+        }
+
+
+    }
+}
--- ObjektoNuskaitymasIsKonsoles/Program.cs
+++ ObjektoNuskaitymasIsKonsoles/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ObjektoNuskaitymasIsKonsoles // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Preke preke1;
+
+            preke1 = NuskaitytiPreke();
+            IsvestiPreke(preke1);
+            
+        }
+
+
+        static Preke NuskaitytiPreke()
+        {
+            Preke preke = new Preke();
+
+            Console.WriteLine("Įveskite prekės pavadinimą: ");
+            string pavadinimas = Console.ReadLine();
+            preke.Pavadinimas = pavadinimas;
+
+            Console.WriteLine("Įveskite prekės aprašymą: ");
+            string aprasymas = Console.ReadLine();
+            preke.Aprasymas = aprasymas;
+
+            Console.WriteLine("Įveskite prekės kainą: ");
+            double kaina = Convert.ToDouble(Console.ReadLine());
+            preke.Kaina = kaina;
+
+            Console.WriteLine("Įveskite prekės id: ");
+            long id = Convert.ToInt64(Console.ReadLine());
+            preke.Id = id;
+
+            return preke;
+
+        }
+
+
+        static void IsvestiPreke(Preke preke)
+        {
+            Console.WriteLine("Pavadinimas: " + preke.Pavadinimas);
+            Console.WriteLine("Aprašymas: " + preke.Aprasymas);
+            Console.WriteLine("Kaina: " + preke.Kaina);
+            Console.WriteLine("Id: " + preke.Id);
         }
     }
 }
